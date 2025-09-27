03acba2698708d6c231f79321481dac542a2d4c6
shahdooz17 <shahdalsayed20042017@gmail.com>
2025-09-27 22:49:16 +0300

    Add solution for LeetCode 1581. Customer Who Visited but Did Not Make Any Transactions

---
+++ 1581. Customer Who Visited but Did Not Make Any Transactions/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+SELECT customer_id,
+       COUNT(*) as count_no_trans
+FROM Visits 
+LEFT JOIN Transactions ON Visits.visit_id = Transactions.visit_id
+WHERE Transactions.visit_id IS NULL
+GROUP BY customer_id
