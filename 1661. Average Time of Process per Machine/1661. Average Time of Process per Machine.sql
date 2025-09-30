c5fc53d917768f6d54b36228281d5fc51cc2313d
shahdooz17 <shahdalsayed20042017@gmail.com>
2025-09-30 23:24:23 +0300

    Add solution for LeetCode 1661. Average Time of Process per Machine

---
+++ 1661. Average Time of Process per Machine/1661. Average Time of Process per Machine.sql
@@ -0,0 +1,6 @@
+select machine_id, 
+round(
+  sum(case when activity_type = 'start' then timestamp*-1 
+  else timestamp end) / count(distinct process_id), 3) as processing_time 
+from Activity
+group by machine_id
